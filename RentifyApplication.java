f22bea299f58662923a0f53ee10097d83730ebe5
Vidhi Yadav <76217390+vidhiyadav04@users.noreply.github.com>
2024-05-26 18:09:35 +0530

    RentifyApplication.java

---
+++ RentifyApplication.java
@@ -0,0 +1,12 @@
+package com.example.rentify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentifyApplication.class, args);
+    }
+}
